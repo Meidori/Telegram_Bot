ff3a5a0c46e358e86e23765abc0d282d08ac9014
Meidori <86147868+Meidori@users.noreply.github.com>
2021-12-19 17:39:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -85,4 +85,20 @@ def get_message(message):
             client.send_message(message.chat.id, list2)
 
 
-client.polling(non_stop=True)           # постоянная работа бота
+@server.route('/' + BOT_TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    client.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    client.remove_webhook()
+    client.set_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
