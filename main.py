e8ac0824e7f5a88e5ab08954f833e8e595af6a9c
Meidori <86147868+Meidori@users.noreply.github.com>
2021-12-19 17:48:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import telebot
+from telebot import types
+import random
+from list1 import list1
+from list2 import list2
+import cfg
+
+client = telebot.TeleBot(cfg.token)
+
+
+@client.message_handler(commands=['start'])     # Команда /start
+def start(message):                             # Функция меню бота
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item_info = types.KeyboardButton('Информация о боте 🤖')
+    item_commands = types.KeyboardButton('Команды ⌨')
+    item_games = types.KeyboardButton('Игры 🎮')
+
+    markup.add(item_info, item_commands, item_games)
+
+    sticker = open('papich.webp', 'rb')
+    # сообщение которое выводится при первом вызове меню (команда /start)
+    client.send_sticker(message.chat.id, sticker)
+    client.send_message(message.chat.id, 'Здравсвуйте, {0.first_name}!'.format(message.from_user), reply_markup=markup)
+
+
+# работа всего бота
+@client.message_handler(content_types=['text'])
+def get_message(message):
+    if message.chat.type == 'private':       # проверяем, что мы в ЛС, а не в канале
+
+        if message.text == 'Информация о боте 🤖':
+            client.send_message(message.chat.id, '''
+@Meidori_bot – это простой игровой Телеграм бот, 
+написанный на языке программирования Python, для проекта на Сириус.
+
+Код можно просмотреть здесь: https://github.com/Meidori/Telegram_Bot
+                            ''')
+
+        elif message.text == 'Команды ⌨':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item_list1 = types.KeyboardButton('1/2 страница команд 🗒')
+            item_list2 = types.KeyboardButton('2/2 страница команд 🗒')
+            item_back = types.KeyboardButton('Назад ⬅')
+
+            markup.add(item_list1, item_list2, item_back)
+
+            client.send_message(message.chat.id, 'Вы в меню: Команды ⌨',
+                                reply_markup=markup)
+
+        elif message.text == 'Игры 🎮':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item_roll = types.KeyboardButton('Случайное число от 1 до 100 🎰')
+            item_dice = types.KeyboardButton('Сыграть в кости 🎲')
+            item_back = types.KeyboardButton('Назад ⬅')
+
+            markup.add(item_roll, item_dice, item_back)
+
+            client.send_message(message.chat.id, 'Вы в меню: Игры 🎮',
+                                reply_markup=markup)
+
+        elif message.text == 'Назад ⬅':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            item_info = types.KeyboardButton('Информация о боте 🤖')
+            item_commands = types.KeyboardButton('Команды ⌨')
+            item_games = types.KeyboardButton('Игры 🎮')
+
+            markup.add(item_info, item_commands, item_games)
+
+            client.send_message(message.chat.id, 'Главное Меню 🗒',
+                                reply_markup=markup)
+
+        elif message.text == 'Случайное число от 1 до 100 🎰':
+            client.send_message(message.chat.id, 'Случайное число(1-100): ' + str(random.randint(1, 100)))
+
+        elif message.text == 'Сыграть в кости 🎲':
+            client.send_message(message.chat.id, 'Вам выпало: ' + str(random.randint(1, 6)) + ' ' + str(random.randint(1, 6)))
+
+        elif message.text == '1/2 страница команд 🗒':
+            client.send_message(message.chat.id, list1)
+        elif message.text == '2/2 страница команд 🗒':
+            client.send_message(message.chat.id, list2)
+
+
+client.polling(none_stop=True)
