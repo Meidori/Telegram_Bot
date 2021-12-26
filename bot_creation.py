b763cab9f05708d016f9e8d1f4c2cd44205a4116
Meidori <86147868+Meidori@users.noreply.github.com>
2021-12-26 16:20:58 +0300

    Delete bot_creation.py

--- bot_creation.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher, types
-import os
-
-TOKEN = "5092314407:AAGFlgQQbTYhL4cfXli6DpaN-He5jtxeRj0"
-# APP_URL = f'https://meidori.herokuapp.com/{TOKEN}'
-
-client = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
-dispatcher = Dispatcher(client)
